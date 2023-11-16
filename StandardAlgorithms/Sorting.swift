55d3ffd8c37f07ccc9710f101c88737daff943e0
Finnian Phelan <phelan.f@etoncollege.org.uk>
2023-11-16 13:22:12 +0000

    Reformatted Sorting and SortingTest

--- StandardAlgorithms/Sorting.swift
+++ StandardAlgorithms/Sorting.swift
@@ -8,67 +8,64 @@
 import Foundation
 
 class Sorting {
-    
-  func bubbleSort(_ inputArray: [Int]) -> [Int] {
-    var inputArray = inputArray
-    for _ in 1...inputArray.count {
-      for j in 1...inputArray.count-1 {
-        let i = j-1
-        let itemAtJ = inputArray[j]
-
-        if inputArray[i] > inputArray[j] {
-          inputArray[j] = inputArray[i]
-          inputArray[i] = itemAtJ
-        }
-      }
-    }
-    return inputArray
-  }
-
-  func mergeSort(_ inputArray: [Int]) -> [Int] {
-    var sortedArray = inputArray
-
-    let array1 = mergeSort(Array((inputArray[0..<inputArray.count/2])))
-    let array2 = mergeSort(Array(inputArray[inputArray.count/2..<inputArray.count]))
-    sortedArray = merge(array1, array2)
-
-    func merge(_ array1: [Int], _ array2: [Int]) -> [Int] {
-      var mergedArray: [Int] = []
-      var leftIndex = 0
-      var rightIndex = 0
-
-      while leftIndex < array1.count && rightIndex < array2.count {
-        let leftElement = array1[leftIndex]
-        let rightElement = array2[rightIndex]
-
-        if leftElement < rightElement {
-          mergedArray.append(leftElement)
-          leftIndex += 1
-        } else if leftElement > rightElement {
-          mergedArray.append(rightElement)
-          rightIndex += 1
-        } else {
-          mergedArray.append(leftElement)
-          leftIndex += 1
-          mergedArray.append(rightElement)
-          rightIndex += 1
-        }
-      }
-
-      while leftIndex < array1.count {
-        mergedArray.append(array1[leftIndex])
-        leftIndex += 1
-      }
-
-      while rightIndex < array2.count {
-        mergedArray.append(array2[rightIndex])
-        rightIndex += 1
-      }
-
-      return mergedArray
-    }
-
-    return sortedArray
-  }
-
+	
+	func bubbleSort(_ inputArray: [Int]) -> [Int] {
+		var inputArray = inputArray
+		for _ in 1...inputArray.count {
+			for j in 1...inputArray.count-1 {
+				let i = j-1
+				let itemAtJ = inputArray[j]
+				
+				if inputArray[i] > inputArray[j] {
+					inputArray[j] = inputArray[i]
+					inputArray[i] = itemAtJ
+				}
+			}
+		}
+		return inputArray
+	}
+	
+	func mergeSort(_ inputArray: [Int]) -> [Int] {
+		var sortedArray = inputArray
+		
+		let array1 = mergeSort(Array((inputArray[0..<inputArray.count/2])))
+		let array2 = mergeSort(Array(inputArray[inputArray.count/2..<inputArray.count]))
+		sortedArray = merge(array1, array2)
+		
+		func merge(_ array1: [Int], _ array2: [Int]) -> [Int] {
+			var mergedArray: [Int] = []
+			var leftIndex = 0
+			var rightIndex = 0
+			
+			while leftIndex < array1.count && rightIndex < array2.count {
+				let leftElement = array1[leftIndex]
+				let rightElement = array2[rightIndex]
+				
+				if leftElement < rightElement {
+					mergedArray.append(leftElement)
+					leftIndex += 1
+				} else if leftElement > rightElement {
+					mergedArray.append(rightElement)
+					rightIndex += 1
+				} else {
+					mergedArray.append(leftElement)
+					leftIndex += 1
+					mergedArray.append(rightElement)
+					rightIndex += 1
+				}
+			}
+			while leftIndex < array1.count {
+				mergedArray.append(array1[leftIndex])
+				leftIndex += 1
+			}
+			while rightIndex < array2.count {
+				mergedArray.append(array2[rightIndex])
+				rightIndex += 1
+			}
+			
+			return mergedArray
+		}
+		
+		return sortedArray
+	}
 }
--- StandardAlgorithmsTests/SortingTest.swift
+++ StandardAlgorithmsTests/SortingTest.swift
@@ -8,9 +8,22 @@
 import XCTest
 
 final class SortingTest: XCTestCase {
-    
-    func testBubbleSort() {
-        XCTAssertEqual(true, true)
-    }
 
+	let testArrays = [
+		(data: [5, 4, 3, 2, 1], expected: [1, 2, 3, 4, 5]),
+		(data: [10, 15, 8, 20, 3], expected: [3, 8, 10, 15, 20]),
+		(data: [8, 1, 7, 12, 20], expected: [1, 7, 8, 12, 20]),
+		(data: [16, 2, 9, 13, 6], expected: [2, 6, 9, 13, 16]),
+		(data: [10, 14, 11, 9, 4], expected: [4, 9, 10, 11, 14]),
+		(data: [7, 5, 8, 19, 10], expected: [5, 7, 8, 10, 19]),
+		(data: [15, 3, 6, 10, 13], expected: [3, 6, 10, 13, 15]),
+		(data: [2, 11, 9, 7, 16], expected: [2, 7, 9, 11, 16]),
+		(data: [8, 1, 9, 5, 12], expected: [1, 5, 8, 9, 12]),
+		(data: [9, 7, 8, 4, 10], expected: [4, 7, 8, 9, 10])
+	]
+	
+	for testCase in testArrays {
+		let actual = mergeSort(testArray)
+		XCTAssertEqual(actual, expected)
+	}
 }
